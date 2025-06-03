198890f3208ae8d2aa203827a963075df735c22e
alvin <alvinsatria452@gmail.com>
2025-06-03 16:57:41 +0700

    initial commit

---
+++ librarymanagementsystem/Main.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package librarymanagementsystem;
+import librarymanagementsystem.ui.LibraryUI;
+/**
+ *
+ * @author Alvin
+ */
+public class Main {
+    public static void main(String[] args){
+    java.awt.EventQueue.invokeLater(() -> {
+            new LibraryUI().setVisible(true);
+        });
+    }
+}
